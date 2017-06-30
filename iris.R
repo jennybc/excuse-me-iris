c1690df90cf38a6b615afd2c50f7dc3565d70350
jennybc <jenny@stat.ubc.ca>
2017-06-30 15:13:47 -0700

    Render as report

--- iris.R
+++ iris.R
@@ -1,3 +1,10 @@
-summary(iris)
+#' ---
+#' title: "Report from R/Rmd"
+#' author: "Jenny Bryan"
+#' date: "`r format(Sys.Date())`"
+#' output: github_document
+#' ---
 
-aggregate(. ~ Species, data = iris, mean)
+#' The iris data is boring, but it won't distract from the Git content.
+
+aggregate(. ~ Species, data = iris, median)
